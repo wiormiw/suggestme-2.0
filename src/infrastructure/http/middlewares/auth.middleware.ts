8b6b715cbcc5e6610b962bb2bf67d8214a078656
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-11-30 23:46:39 +0700

    chore(http): move into infrastructure

---
+++ src/infrastructure/http/middlewares/auth.middleware.ts
@@ -0,0 +1,39 @@
+import { Elysia } from 'elysia';
+import { AuthContext } from '@/types/context';
+
+import { AppError } from '@/common/errors/app.error';
+import { AuthUser } from '@/common/schemas/common.schema';
+import { jwtAccess } from '@/common/utils/setup.jwt';
+
+export const auth = new Elysia({ name: 'auth' })
+  .use(jwtAccess)
+  .derive({ as: 'global' }, async ({ cookie: { access_token }, headers, jwtAccess }) => {
+    let user: AuthUser | undefined;
+    const accesTokenCookie = access_token?.value;
+    const headerValue = headers.authorization?.slice(7);
+    const token = (accesTokenCookie as string | undefined) || headerValue;
+
+    if (token) {
+      const payload = await jwtAccess.verify(token);
+      if (payload) {
+        user = payload as AuthUser;
+      }
+    }
+
+    return { user };
+  });
+
+export const ensureAuth = ({ user }: AuthContext) => {
+  if (!user) {
+    throw new AppError('UNAUTHORIZED', 'Authentication required.', 401);
+  }
+};
+
+export const ensureAdmin = ({ user }: AuthContext) => {
+  if (!user) {
+    throw new AppError('UNAUTHORIZED', 'Authentication required.', 401);
+  }
+  if (user.role !== 'admin') {
+    throw new AppError('FORBIDDEN', 'Admin access required.', 403);
+  }
+};
