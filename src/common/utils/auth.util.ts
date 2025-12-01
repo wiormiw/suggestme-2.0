6401fe93864d97f6c8c7a9d48ea24e5a68e4e186
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-12-01 16:05:55 +0700

    refactor: auth util uses es6 instead wrapped in abstract class

--- src/common/utils/auth.util.ts
+++ src/common/utils/auth.util.ts
@@ -1,9 +1,7 @@
-export abstract class AuthUtil {
-  static async hashPassword(password: string): Promise<string> {
-    return await Bun.password.hash(password, { algorithm: 'bcrypt', cost: 10 });
-  }
+export const hashPassword = async (password: string): Promise<string> => {
+  return await Bun.password.hash(password, { algorithm: 'bcrypt', cost: 10 });
+};
 
-  static async verifyPassword(password: string, hash: string): Promise<boolean> {
-    return await Bun.password.verify(password, hash);
-  }
-}
+export const verifyPassword = async (password: string, hash: string): Promise<boolean> => {
+  return await Bun.password.verify(password, hash);
+};
