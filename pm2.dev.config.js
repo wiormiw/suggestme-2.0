f9aa26ba8de30459ab7a61b6a82de24e0dd5ba43
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-12-01 16:05:26 +0700

    chore: prettier

--- pm2.dev.config.js
+++ pm2.dev.config.js
@@ -3,21 +3,21 @@
  */
 module.exports = {
   // Dev
-  name: "suggest-me-dev",
-  
+  name: 'suggest-me-dev',
+
   // The application file to execute
-  script: "src/index.ts",
-  
+  script: 'src/index.ts',
+
   // Use 'bun' as the interpreter to run the TypeScript file directly
-  interpreter: "bun",
-  
+  interpreter: 'bun',
+
   // Ignore node_modules for watching performance
-  ignore_watch: ["node_modules"],
-  
+  ignore_watch: ['node_modules'],
+
   // Standard execution mode
-  exec_mode: "fork",
-  
+  exec_mode: 'fork',
+
   // Optional: Log files for development environment
-  error_file: "logs/dev-err.log",
-  out_file: "logs/dev-out.log",
+  error_file: 'logs/dev-err.log',
+  out_file: 'logs/dev-out.log',
 };
